204717bb135c4c6db201d724b2877d79492a1cf7
Natascha <NataschaHof@gmx.de>
2020-12-03 22:32:31 +0100

    heroku test

--- src/Authentication/verifyAuth.js
+++ src/Authentication/verifyAuth.js
@@ -1,11 +1,15 @@
+const cookie = require("cookie");
 const jwt = require("jsonwebtoken");
 const fs = require("fs");
 const publicKey = fs.readFileSync("public.pem");
 
 module.exports = async (req, res, next) => {
+  const cookies = cookie.parse(req.headers.cookie || "");
+  console.log("cookies", cookies);
   // verify the validity of the access token
   try {
-    const checkValidity = await jwt.verify(req.cookies.accessToken, "cat");
+    const checkValidity = await jwt.verify(cookies.accessToken, "cat");
+    /* const checkValidity = await jwt.verify(req.cookies.accessToken, "cat"); */
 
     // if valid go next
     if (checkValidity) {
